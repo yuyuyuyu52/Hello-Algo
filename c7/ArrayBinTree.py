00b353adb4444d4f24589ec6710be71601f4412c
yuyuyuyu52 <will29982@gmail.com>
2024-06-28 12:51:34 +0800

    update some thing

--- c7/ArrayBinTree.py
+++ c7/ArrayBinTree.py
@@ -1,1 +1,18 @@
+class ArrayBinTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
+    def insert(self, value):
+        if self.root == None:
+            self.root = ArrayBinTree()
+            self.size = 1
+            return True
+
+        if value < self.root:
+            self.insert(value)
+            return True
+
+    def search(self, value):
+        if self.root == None:
+            return False
